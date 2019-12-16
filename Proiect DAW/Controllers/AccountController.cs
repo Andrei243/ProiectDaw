61f10cb4548bb3c25f74d2b4e53e46b230616ece
Sandu Daniela <daniela.sandu122@gmail.com>
2019-12-16 19:05:01 +0200

    Successfull login, problem with current user

--- Proiect DAW/Controllers/AccountController.cs
+++ Proiect DAW/Controllers/AccountController.cs
@@ -96,7 +96,7 @@ namespace Proiect_DAW.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Feed");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
